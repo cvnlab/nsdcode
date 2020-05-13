337d6db8490ea4b60fca74d684be278eb41fc5c5
Kendrick Kay <kendrick@post.harvard.edu>
2020-05-13 18:17:04 -0500

    remove obsolete content

--- matlab/cvn/examples_cvnlookup2.m
+++
@@ -1,8 +0,0 @@
-%% This document demonstrates quick-and-dirty usage of cvnlookup.m.
-
-% here we start with volume-oriented data, do a quick mapping to the
-% mid-gray surface, and then use cvnlookup.m to visualize the results.
-sourcedata = sprintf('%s/ppdata/subj01/func1mm/R2.nii.gz',nsd_datalocation);
-data =  nsd_mapdata(1,'func1pt0','lh.layerB2',sourcedata,'linear');
-data2 = nsd_mapdata(1,'func1pt0','rh.layerB2',sourcedata,'linear');
-cvnlookup('subj01',1,[data; data2],[0 50],hot(256));
