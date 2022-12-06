fix: off-by-one error at edge of volume
Closes https://github.com/cvnlab/nsdcode/issues/21.

Fixes an off-by-one error caused by conversion from MATLAB to Python indexing.

nsdcode/interp_wrapper.py
@@ -56,18 +56,18 @@ def interp_wrapper(vol, coords, interptype='cubic'):
 
     # bad locations must get set to NaN
     bad = np.any(isnotfinite(coords), axis=0)
-    coords[:, bad] = 1
+    coords[:, bad] = 0
 
     # out of range must become NaN, too
     bad = np.any(
         np.c_[
             bad,
-            coords[0, :] < 1,
-            coords[0, :] > vol.shape[0],
-            coords[1, :] < 1,
-            coords[1, :] > vol.shape[1],
-            coords[2, :] < 1,
-            coords[2, :] > vol.shape[2]], axis=1).astype(bool)
+            coords[0, :] < 0,
+            coords[0, :] >= vol.shape[0],
+            coords[1, :] < 0,
+            coords[1, :] >= vol.shape[1],
+            coords[2, :] < 0,
+            coords[2, :] >= vol.shape[2]], axis=1).astype(bool)
 
     # resample the volume
     if not np.any(np.isreal(vol)):